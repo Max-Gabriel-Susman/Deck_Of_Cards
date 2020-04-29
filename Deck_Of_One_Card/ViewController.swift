c0ebedd7c9d7dda131e9b51b3519ed0b4dbfe43d
Moses <gabe.susman@gmail.com>
2020-04-28 19:18:59 -0600

    Initial Commit

---
+++ Deck_Of_One_Card/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deck_Of_One_Card
+//
+//  Created by Moses on 4/28/20.
+//  Copyright © 2020 Max Susman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
